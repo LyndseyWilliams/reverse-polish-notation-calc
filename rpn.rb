e58f1e6522f89fd4da10958fdded07b84dd0d55b
Lyndsey Williams <lywilliams@gmail.com>
2014-06-27 08:34:26 -0700

    adds rpn calculator

--- rpn.rb
+++ rpn.rb
@@ -1,12 +1,82 @@
 
 
- 
-puts calculate_rpn("1 2 +") == 3 
-puts calculate_rpn("4 2 /") == 2 
-puts calculate_rpn("2 3 4 + *") == 14 
-puts calculate_rpn("3 4 + 5 6 + *") == 77 
-puts calculate_rpn("13 4 -") == 9 
-puts calculate_rpn("1 +")  == "not enough arguments"
-puts calculate_rpn("a b +") == "invalid number" 
- 
+class RPNCalculator
+  def initialize
+    @inputs = []
+  end
+  
+  def plus
+    @inputs.push(@inputs.pop + @inputs.pop)
+  end
+  
+  def minus
+    var1= @inputs.pop
+    var2= @inputs.pop
+    @inputs.push(var2-var1)
+  end
+  
+  def divide
+    var1 = pop.to_f
+    var2 = pop.to_f
+    @inputs.push(var2/var1)
+  end
+  
+  def times
+    @inputs.push(@inputs.pop * @inputs.pop)
+  end
+  
+  def value
+    @inputs.last
+  end
+  
+  def operators(string)
+    string.split(' ').map do |e|
+      begin
+        Integer(e)
+      rescue
+        e.to_sym
+      end
+    end
+  end
+  
+  def evaluate(string)
+    operators(string).each do |x|
+      begin
+        @inputs.push(Integer(x))
+      rescue
+        case x
+        when :+
+          plus
+        when :-
+          minus
+        when :*
+          times
+        when :/
+          divide
+      end
+    end
+  end
+  value
+ end
+end
+
+
+def convert_string_to_num
+end
+
+calculator = RPNCalculator.new
+puts calculator.evaluate('2 2 +')
+
+calculator2 = RPNCalculator.new
+puts calculator2.evaluate("3 4 + 5 6 + *")
+
+
+
+
+
+
+
+
+
+
 
